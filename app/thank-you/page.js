ce01faa94aa73a773e18754d69394bcebaa1505b
chaitanyagohar <chaitanyagohar@gmail.com>
2025-09-24 13:16:07 +0530

    favicon

--- app/thank-you/page.js
+++ app/thank-you/page.js
@@ -9,7 +9,7 @@ export default function ThankYouPage() {
         Thank You!
       </h1>
       <p className="font-montserrat text-lg text-gray-700 mb-8 max-w-md">
-        Your request for the brochure has been received. Our team will get in touch with you shortly.
+        Your request for the brochure has been received. It will be sent to your email address shortly.
       </p>
       <Link href="/" className="px-6 py-3 bg-blue-600 text-white font-semibold rounded-md hover:bg-blue-700 transition-colors">
         Back to Home
